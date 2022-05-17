0a784fcf751eb5ef9b4c35dbb9a6abc7f8c7a594
abdullahalshawafi <abdullahadel.aam@gmail.com>
2022-05-17 21:25:33 +0200

    Added search bar to home page

---
+++ src/components/Search.vue
@@ -0,0 +1,52 @@
+<template>
+  <form>
+    <label for="country">
+      <i class="fa fa-search"></i>
+      <input
+        type="text"
+        name="country"
+        placeholder="Search for a country..."
+        id="country"
+        autocomplete="off"
+      />
+    </label>
+  </form>
+</template>
+
+<script>
+export default {
+  name: "search-component",
+};
+</script>
+
+<style lang="scss" scoped>
+@import "@/assets/scss/variables";
+
+form {
+  background-color: var(--elements-color);
+  box-shadow: 0 0px 10px -3px rgb(0 0 0 / 20%);
+  padding: 1rem 1.5rem;
+  border-radius: 5px;
+  width: 35%;
+
+  label {
+    display: flex;
+
+    i {
+      color: var(--text-color);
+    }
+
+    input {
+      width: 100%;
+      border: none;
+      background-color: transparent;
+      color: var(--input-color);
+      margin-left: 1.5rem;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,27 @@
 <template>
-  <div></div>
+  <main>
+    <div class="search-filter-wrapper">
+      <Search />
+    </div>
+  </main>
 </template>
 
 <script>
+import Search from "@/components/Search.vue";
 export default {
   name: "HomeView",
+  components: { Search },
 };
 </script>
+
+<style lang="scss" scoped>
+main {
+  font-size: 14px;
+}
+.search-filter-wrapper {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 5rem 0 5rem;
+}
+</style>
