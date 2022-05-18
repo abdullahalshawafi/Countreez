b625a8e65f96c6d7d3db4f1c6a983de97945be30
abdullahalshawafi <abdullahadel.aam@gmail.com>
2022-05-18 22:03:41 +0200

    Added a loader component

---
+++ src/components/Loader.vue
@@ -0,0 +1,31 @@
+<template>
+  <loading
+    v-model:active="isLoading"
+    :is-full-page="false"
+    loader="bars"
+    color="#111517"
+  />
+</template>
+
+<script>
+import { ref, watchEffect } from "vue";
+import Loading from "vue-loading-overlay";
+import "vue-loading-overlay/dist/vue-loading.css";
+
+export default {
+  name: "loader-component",
+  props: ["loading"],
+  components: { Loading },
+  setup(props) {
+    const isLoading = ref(true);
+
+    watchEffect(() => {
+      isLoading.value = props.loading;
+    });
+
+    return { isLoading };
+  },
+};
+</script>
+
+<style></style>
